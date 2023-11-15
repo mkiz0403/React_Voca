2a8b7476a983ab548e01697094d9818cf99c1466
Taejeong <mkiz0403@gmail.com>
2023-11-15 18:42:19 +0900

    Checkbox on/off

--- src/component/Word.jsx
+++ src/component/Word.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
-export default function Word({word}){
+import "./Word.css"
+export default function Word({ word }){
     const[isShow, setIsShow] = useState(false);
+    const[isDone, setIsDone] = useState(word.isDone);
+    
 
     const toggleShow = () => {
       setIsShow(!isShow)
     }
+
+    const toggleIsDone = () => {
+      setIsDone(!isDone)
+    }
+    
   return (
     <>
-      <tr>
+      <tr className={isDone ? "off" : ""} >
         <td>
-            <input type="checkbox" />
+            <input type="checkbox" checked={isDone} onChange={toggleIsDone}/>
         </td>
         <td>{word.eng}</td>
         <td>{isShow && word.kor}</td>
-        <td>
-            <button onClick={toggleShow}className='view'>뜻 보기</button>
+        <td> 
+            <button onClick={toggleShow} className='view'>
+              뜻 {isShow ? "숨기기" : "보기"}</button>
             <button className='del'>삭제</button>
         </td>
       </tr>
